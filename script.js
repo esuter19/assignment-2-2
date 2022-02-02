1b830a10ec64f2941887761b1556fcef14dd053a
esuter19 <59849377+esuter19@users.noreply.github.com>
2022-02-02 11:42:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var map = L.map('mapid').setView([37.760109273244524, -122.4414471468871], 16);
+var map = L.map('mapid').setView([37.760109273244524, -122.4414471468871], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -9,5 +9,9 @@ $.getJSON("sf_crime.geojson",function(data){
 		iconUrl: "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/43207/oncoming-police-car-emoji-clipart-md.png",
 		iconSize: [40,40]
 	});
-    L.geoJson(data).addTo(map);
+    L.geoJson(data ,{
+    pointToLayer: function(feature,latlng){
+	  return L.marker(latlng,{icon: ratIcon});
+    }
+  } ).addTo(map);
 });
